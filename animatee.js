f08871f8b6bb9df2b3ce6ab1b0f014c48b7dd581
Olivana <110850743+0livana@users.noreply.github.com>
2022-12-22 18:00:38 +0100

    Add files via upload

---
+++ animatee.js
@@ -0,0 +1,34 @@
+
+function bounce(){
+    document.querySelector('h1').classList.toggle("animate__bounce");
+}
+function swing(){
+    document.querySelector('h1').classList.toggle("animate__swing");
+}
+function wobble(){
+    document.querySelector('h1').classList.toggle("animate__wobble");
+}
+function flip(){
+    document.querySelector('h1').classList.toggle("animate__flip");
+}
+function backInDown(){
+    document.querySelector('h1').classList.toggle("animate__backInDown");
+}
+function backInUp(){
+    document.querySelector('h1').classList.toggle("animate__backInUp");
+}
+function backInLeft(){
+    document.querySelector('h1').classList.toggle("animate__backInLeft");
+}
+function backInRight(){
+    document.querySelector('h1').classList.toggle("animate__backInRight");
+}
+function fadeInTopLeft(){
+    document.querySelector('h1').classList.toggle("animate__fadeInTopLeft");
+}
+function fadeInBottomRight(){
+    document.querySelector('h1').classList.toggle("animate__fadeInBottomRight");
+}
+function backInDown(){
+    document.querySelector('h1').classList.toggle("animate__backInDown");
+}
